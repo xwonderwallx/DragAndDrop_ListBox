f5e128b0de0ef5caa233b6316fac8f61041b0fd5
ACER <ACER@DESKTOP-GDVFM0R>
2022-11-03 19:24:42 +0200

    Add project files.

---
+++ DragAndDrop_ListBox/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DragAndDrop_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbItems = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbItems
+            // 
+            this.lbItems.AllowDrop = true;
+            this.lbItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbItems.FormattingEnabled = true;
+            this.lbItems.ItemHeight = 20;
+            this.lbItems.Location = new System.Drawing.Point(0, 0);
+            this.lbItems.Name = "lbItems";
+            this.lbItems.Size = new System.Drawing.Size(800, 450);
+            this.lbItems.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbItems);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbItems;
+    }
+}
---
+++ DragAndDrop_ListBox/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragAndDrop_ListBox
+{
+    public partial class Form1 : Form
+    {
+        int _y = 0;
+        int _x = 0;
+        int _count = 0;
+        bool _flag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lbItems.DragDrop += LbItems_DragDrop;
+            lbItems.DragEnter += LbItems_DragEnter;
+        }
+
+        private void LbItems_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void LbItems_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (Path.GetExtension(files[i]) != ".jpg" &&
+                    Path.GetExtension(files[i]) != ".png" &&
+                    Path.GetExtension(files[i]) != ".bmp" &&
+                    Path.GetExtension(files[i]) != ".gif")
+                {
+                    MessageBox.Show("Incorrect Format! Only Images allow!");
+                    continue;
+                }
+
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox.Size = new Size(300, 300);
+                pictureBox.Location = new Point(_x, _y);
+                ChangePosition();
+
+                Bitmap bitmap = new Bitmap(files[i]);
+                pictureBox.Image = bitmap;
+                lbItems.Controls.Add(pictureBox);
+            }
+        }
+
+        private void ChangePosition()
+        {
+            if(_flag)
+            {
+                _x = 0;
+            }
+            else
+            {
+                _x += 300;
+            }
+            _count++;
+
+            if (_count == 2)
+            {
+                _count = 0;
+                _y += 300;
+            }
+
+            _flag = !_flag;
+        }
+    }
+}
